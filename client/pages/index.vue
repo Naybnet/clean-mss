8add8fb1f08b59779bb347f6a8a33a50cf7e7c1b
naybnet <6021316+Naybnet@users.noreply.github.com>
2024-06-06 21:36:22 +0200

    initial

---
+++ client/pages/index.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'default',
+})
+
+useHead({ title: 'Page d’accueil - Gabarit de démarrage VueDsfr' })
+</script>
+
+<template>
+  <div class="fr-container fr-mb-5w">
+    <h1 class="fr-mt-3w fr-mt-md-5w fr-mb-5w">
+      Accueil
+    </h1>
+
+    <p>
+      <NuxtLink to="/apropos">
+        À propos
+      </NuxtLink>
+    </p>
+
+    <p>
+      <span class="fr-icon-ancient-gate-fill" /> <!-- Icône DSFR -->
+       (<code>&lt;span class="fr-icon-ancient-gate-fill" /></code>)
+    </p>
+    <p>
+      <VIcon name="ri-flag-line" />  <!-- Icône OhVueIcon -->
+       (<code>&lt;VIcon name="ri-flag-line" /></code>)
+    </p>
+    <p>
+      <OhVueIcon name="ri-flag-line" />  <!-- Icône OhVueIcon -->
+       (<code>&lt;OhVueIcon name="ri-flag-line" /></code>)
+    </p>
+  </div>
+</template>
